939fd246d4b03b99ff8bfc9f670d4760734ad025
Ivan Reshetnikov <resivalex@gmail.com>
2021-08-01 18:51:14 +0300

    Hide datepickers when month is selected

--- web/src/DateRange.js
+++ web/src/DateRange.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Grid from '@material-ui/core/Grid'
 import MomentUtils from '@date-io/moment'
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
@@ -10,41 +10,47 @@ import _ from 'lodash'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 
-const monthSelectLimit = 6
+const monthSelectLimit = 12
 const dateFormat = 'yyyy-MM-DD'
 
 class DateRange extends Component {
+  state = { showDatePickers: false }
+
   render() {
     return (
       <MuiPickersUtilsProvider utils={MomentUtils}>
         <Grid container spacing={1}>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={this.state.showDatePickers ? 4 : 12}>
             {this.renderMonthsSelect()}
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <KeyboardDatePicker
-              style={{width: '100%'}}
-              value={this.props.dateFrom}
-              onChange={(date) => this.props.onDateFromChange(date.toDate())}
-              format={dateFormat}
-              variant="inline"
-              inputVariant="outlined"
-              label="От"
-              autoOk
-            />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <KeyboardDatePicker
-              style={{width: '100%'}}
-              value={this.props.dateTo}
-              onChange={(date) => this.props.onDateToChange(date.toDate())}
-              format={dateFormat}
-              variant="inline"
-              inputVariant="outlined"
-              label="До"
-              autoOk
-            />
-          </Grid>
+          {this.state.showDatePickers && (
+            <Fragment>
+              <Grid item xs={12} sm={4}>
+                <KeyboardDatePicker
+                  style={{ width: '100%' }}
+                  value={this.props.dateFrom}
+                  onChange={(date) => this.props.onDateFromChange(date.toDate())}
+                  format={dateFormat}
+                  variant="inline"
+                  inputVariant="outlined"
+                  label="От"
+                  autoOk
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <KeyboardDatePicker
+                  style={{ width: '100%' }}
+                  value={this.props.dateTo}
+                  onChange={(date) => this.props.onDateToChange(date.toDate())}
+                  format={dateFormat}
+                  variant="inline"
+                  inputVariant="outlined"
+                  label="До"
+                  autoOk
+                />
+              </Grid>
+            </Fragment>
+          )}
         </Grid>
       </MuiPickersUtilsProvider>
     )
@@ -57,7 +63,7 @@ class DateRange extends Component {
           Месяц
         </InputLabel>
         <Select value={this.currentMonthDelta()} onChange={(value) => this.setMonth(value.target.value)}>
-          <MenuItem value="custom_period">...</MenuItem>
+          <MenuItem value="custom_period">выбрать даты...</MenuItem>
           {_.times(monthSelectLimit, (monthsAgo) => {
             return (
               <MenuItem key={monthsAgo} value={monthsAgo}>
@@ -85,7 +91,10 @@ class DateRange extends Component {
 
   setMonth(monthsAgo) {
     const range = this.monthPeriods()[monthsAgo]
-    if (!range) {
+    if (range) {
+      this.setState({ showDatePickers: false })
+    } else {
+      this.setState({ showDatePickers: true })
       return
     }
     this.props.onDateFromChange(range['dateFrom'])
